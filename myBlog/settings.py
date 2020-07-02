6637a54a189a6f79d36166c3886e855da5259070
raed <eng.salhab@gmail.com>
2020-07-02 13:40:46 +0300

    Create Model

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mystore_db',
+            'USER': 'developer',
+            'PASSWORD': '0568547489',
+            'HOST': '127.0.0.1',
+            'PORT': '5432','ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mystore_db',
+            'USER': 'developer',
+            'PASSWORD': '0568547489',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
     }
 }
 
@@ -105,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Hebron'
 
 USE_I18N = True
 
--- news/models.py
+++ news/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    article_text = models.CharField(max_length=2000)
+    pub_date = models.DateTimeField('date published')
