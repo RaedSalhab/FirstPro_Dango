758c2265d6af840db5eb90f11ad777b3b32a3bec
raed <eng.salhab@gmail.com>
2020-07-03 07:57:45 +0300

    Template

--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, 'news/index.html')
+
+def detail(request):
+    return render(request, 'news/detail.html')
