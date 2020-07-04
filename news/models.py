706546c729bc8fa65fbd5cbed52b3a701d2ff753
raed <eng.salhab@gmail.com>
2020-07-04 11:44:22 +0300

    Main action for view index and detail pages

--- news/models.py
+++ news/models.py
@@ -5,9 +5,10 @@ from django.utils import timezone
 # Create your models here.
 class Article(models.Model):
     article_title = models.CharField(max_length=100, default='SOME STRING')
-    article_text = models.CharField(max_length=2000)
+    article_text = models.TextField()
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.article_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
--- news/views.py
+++ news/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from .models import Article
 # Create your views here.
-def index(request):
-    obj = Article.objects.get(id=6)
+def index(request): #Main page view
+    list_obj = Article.objects.all().order_by('-id')[:5]
+    context = {
+        'list_obj': list_obj
+    }
+    return render(request, 'news/index.html', context)
+
+def detail(request,article_id): #Articles view
+    obj = Article.objects.get(id=article_id)
     context = {
         'article_title':obj.article_title,
         'article_text': obj.article_text,
         'pub_date':obj.pub_date,
     }
-    return render(request, 'news/index.html', context)
-
-def detail(request):
-    return render(request, 'news/detail.html')
+    return render(request, 'news/detail.html',context)
