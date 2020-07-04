1fc6066c4195cebb70087739b12a731b3bf0939c
raed <eng.salhab@gmail.com>
2020-07-04 13:10:23 +0300

    Update index.html

--- news/models.py
+++ news/models.py
@@ -7,6 +7,7 @@ class Article(models.Model):
     article_title = models.CharField(max_length=100, default='SOME STRING')
     article_text = models.TextField()
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.article_text
     def was_published_recently(self):
